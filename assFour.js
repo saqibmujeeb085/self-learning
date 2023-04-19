72548cc5e9f13bca89c2b59b13a5c68a2e48c036
saqibmujeeb085 <121731750+saqibmujeeb085@users.noreply.github.com>
2023-04-19 14:47:46 +0500

    ass 4

---
+++ assFour.js
@@ -0,0 +1,42 @@
+// let h1 = document.querySelector("h1");
+// let arr = [
+//     'Example Domain',
+//     'First Click',
+//     'Second Click',
+//     'Third Click'
+// ]
+// function clickHandle() {
+//     switch(h1.innerText){
+//         case arr[0]:
+//             document.h1.innerText = arr[1]
+//             break;
+//         case arr[1]:
+//             document.h1.innerText = arr[2]
+//             break;
+//         case arr[2]:
+//             document.h1.innerText = arr[3]
+//             break;
+//             dfault:
+//             document.h1.innerText = arr[0]
+//     }
+// }
+// h1.addEventListener("click", clickHandle);
+
+var h1 = document.querySelector("h1");
+var arr = ["Example Domain", "First Click", "Second Click", "Third Click"];
+function handleClicks() {
+  switch (h1.innerText) {
+    case arr[0]:
+      h1.innerText = arr[1];
+      break;
+    case arr[1]:
+      h1.innerText = arr[2];
+      break;
+    case arr[2]:
+      h1.innerText = arr[3];
+      break;
+    default:
+      h1.innerText = arr[0];
+  }
+}
+h1.addEventListener("click", handleClicks);
